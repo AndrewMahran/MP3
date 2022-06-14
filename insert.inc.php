d26a7b8fe32fc8c7cf34680dcedd5e3a67589303
AndrewMahran <76974655+AndrewMahran@users.noreply.github.com>
2022-06-14 11:15:37 -0700

    Delete insert.inc.php

--- insert.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once 'dbh.inc.php';
-$name = $_POST['username'];
-$sql = "INSERT INTO clicker.clicks (username) VALUES ('$name');";
-mysqli_query($conn, $sql);
-header("Location: ../index.php");
